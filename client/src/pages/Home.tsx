8cbd2202d1b5862d99d644bc4f18492dea309d86
altx3 <alteratel@gmail.com>
2025-04-25 21:07:19 +1200

    Added a button to navigate monitoring, new favicon and credits, will implement a proper page later for credits

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,27 @@
-import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
 import DefaultBox from "../components/common/DefaultBox";
 import LobbyManager from "../components/LobbyManager";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
   return (
-    <DefaultBox>
+    <DefaultBox sx={{ gap: "1rem" }}>
       <Typography variant="h2" color="primary">
         Multisweeper++ 💣
       </Typography>
       <LobbyManager />
+      <Button
+        variant="contained"
+        onClick={() => handleNavigation("/monitoring")}
+      >
+        monitoring
+      </Button>
     </DefaultBox>
   );
 }
---
+++ credits.md
@@ -0,0 +1,3 @@
+this website: altx3!!!!! (≧▽≦)/
+
+icon pixel art megumin: ummm idk i found it as someones twitter profile picture, can't find original artist through google image search, unless its the twitter person https://x.com/zhezhi4
